f4fd747a446ee9b86d3acdb017c41cc3f9592ccc
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-04-29 22:44:57 +0530

    again hosting the server after fixing the bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
+const port=process.env.PORT || 5000
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "*",
@@ -25,4 +26,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(port, () => console.log("server is running on port 5000"))
